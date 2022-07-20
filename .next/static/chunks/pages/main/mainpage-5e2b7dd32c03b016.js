e9443ab6618226b747eee26cac41581630b535e8
web3mon <hello@web3mon.io>
2022-07-20 09:35:06 +0900

    image modification

--- .next/static/chunks/pages/main/mainpage-5e2b7dd32c03b016.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[622],{7350:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/main/mainpage",function(){return u(5476)}])}},function(n){n.O(0,[92,476,774,888,179],(function(){return _=7350,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
